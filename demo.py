2bb1cdb3bec0a98206b4910fa6f25a2a9b963d21
Deckard-2049-cyber <surajprasadsingh90@gmail.com>
2025-03-23 09:42:42 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -22,4 +22,7 @@
 
 # --------------------------------------------------------------------------------
 
+from src.pipline.training_pipeline import TrainPipeline
 
+pipline = TrainPipeline()
+pipline.run_pipeline()
